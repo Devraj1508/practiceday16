ff9962f559992457157b88cff4e2084d492f0702
Devraj <your-email@example.com>
2026-08-23 22:56:00 +0530

    login and registered integrated with backend and frontend.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,17 @@ const postRouter=require("./Routers/post.router");
 const followRouter=require("./Routers/User.follow");
 const likeRouter=require("./Routers/User.like");
 const cookieParser=require("cookie-parser");
+const cors=require("cors");
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use("/api/auth",authRouter);
 app.use("/api/post",postRouter);
 app.use("/api/follow",followRouter);
